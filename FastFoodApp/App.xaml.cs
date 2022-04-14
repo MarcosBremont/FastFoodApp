167db9dcdaeda95dcf451f578f704f531cee2dea
Marcos Bremont <bremont123@hotmail.com>
2022-04-14 08:33:22 -0400

    Commit Inicial

---
+++ FastFoodApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FastFoodApp.Pantallas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FastFoodApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
