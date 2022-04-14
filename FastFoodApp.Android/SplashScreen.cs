5b9b84084d486f29b8a1aa02fe9dce88ee82d335
Marcos Bremont <bremont123@hotmail.com>
2022-04-14 11:59:50 -0400

    Se agregó el icono tambien y se esta trabajando en el splash screen

---
+++ FastFoodApp.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FastFoodApp.Droid
+{
+    [Activity(Label = "Fast Food App", MainLauncher = true, NoHistory = true, Theme = "@style/splashscreen")]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            Finish();
+        }
+    }
+}
---
+++ FastFoodApp/Metodos/Metodos.cs
@@ -0,0 +1,51 @@
+﻿using FastFoodApp.Configuracion;
+using FastFoodApp.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFoodApp.Metodos
+{
+    public class Metodos
+    {
+        Herramientas herramientas = new Herramientas();
+
+        public Metodos()
+        {
+            // constructor
+        }
+
+        //public async Task<List<EVerbos>> GetListadoVerbos()
+        //{
+        //    var result = await herramientas.EjecutarSentenciaEnApiLibre($"Verbs/ObtenerVerbos");
+        //    var listado_verbos = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EVerbos>>(result);
+
+        //    return listado_verbos;
+        //} // Fin del método ObtenerVerbos
+
+        //public async Task<List<EPosiciones>> GetListadoDePosiciones()
+        //{
+        //    var result = await herramientas.EjecutarSentenciaEnApiLibre($"Verbs/Posiciones");
+        //    var listado_Posiciones = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EPosiciones>>(result);
+
+        //    return listado_Posiciones;
+        //} // Fin del método ObtenerTablaDePosiciones
+
+
+
+        public async Task<Result> EnterToTheTournament(string nombrePersona, int numeroVerbosCorrectos, string direccion)
+        {
+            var result = await herramientas.EjecutarSentenciaEnApiLibre($"Verbs/EnterToTheTournament/{nombrePersona.ToUpper()}/{numeroVerbosCorrectos}/{direccion.ToUpper()}");
+            var response = Newtonsoft.Json.JsonConvert.DeserializeObject<Result>(result);
+            return response;
+        }
+
+        public async Task<Result> SendEmails(string email)
+        {
+            var result = await herramientas.EjecutarSentenciaEnApiLibre($"Verbs/SaveEmailNews/{email.ToUpper()}");
+            var response = Newtonsoft.Json.JsonConvert.DeserializeObject<Result>(result);
+            return response;
+        }
+    }
+}
