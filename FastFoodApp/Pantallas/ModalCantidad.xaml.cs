cdd423e94773b1dd8c3a7a6a9504cb413dadd5ba
marcosbremont <bremont123@hotmail.com>
2022-05-16 11:37:06 -0400

    .

---
+++ FastFoodApp/Pantallas/ModalCantidad.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FastFoodApp.Pantallas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalCantidad : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public event EventHandler OnLLamarOtraPantalla;
+
+        public ModalCantidad()
+        {
+            InitializeComponent();
+            LblTotal.Text = App.Precio.ToString();
+        }
+
+        void BtnagregarCantidad_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void BtnCancelar_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void TxtCantidad_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(TxtCantidad.Text))
+            {
+                LblTotal.Text = App.Precio.ToString();
+            }
+            else
+            {
+                int totalcantidad = Convert.ToInt32(TxtCantidad.Text);
+                LblTotal.Text = (App.Precio * totalcantidad).ToString();
+            }
+        }
+    }
+}
